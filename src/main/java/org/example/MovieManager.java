bf3f40404d8cf13a68a1016646d1faa31299e2a1
dps <dobro.polya@yandex.ru>
2023-03-28 16:23:06 +0300

    add class

---
+++ src/main/java/org/example/MovieManager.java
@@ -0,0 +1,43 @@
+package org.example;
+
+public class MovieManager {
+    String[] title = new String[0];
+    private int limit;
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public MovieManager() {
+        this.limit = 10;
+    }
+
+    public void addMovie(String movieTitle) {
+        String[] tmp = new String[title.length + 1];
+        for (int i = 0; i < title.length; i++) {
+            tmp[i] = title[i];
+        }
+        tmp[tmp.length - 1] = movieTitle;
+        title = tmp;
+    }
+
+    public String[] findAll() {
+        return title;
+
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (title.length < limit) {
+            resultLength = title.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = title[title.length - 1 - i];
+        }
+        return tmp;
+    }
+
+}
---
+++ src/test/java/MovieManagerTest.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.example.MovieManager;
+
+public class MovieManagerTest {
+    @Test
+    public void testAdd() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        manager.addMovie("Номер один");
+        manager.addMovie("Операция Фортуна");
+        manager.addMovie("Моя пиратская свадьба");
+        manager.addMovie("Тайна Келлс");
+        String[] expected = {"Бладшот", "Вперед", "Отель Белград", "Джентельмены", "Человек-неведимка", "Тролли", "Номер один", "Операция Фортуна", "Моя пиратская свадьба", "Тайна Келлс"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLast() {
+        MovieManager manager = new MovieManager();
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        manager.addMovie("Человек-неведимка");
+        manager.addMovie("Тролли");
+        manager.addMovie("Номер один");
+        manager.addMovie("Операция Фортуна");
+        manager.addMovie("Моя пиратская свадьба");
+        manager.addMovie("Тайна Келлс");
+        manager.findLast();
+        String[] expected = {"Тайна Келлс", "Моя пиратская свадьба", "Операция Фортуна", "Номер один", "Тролли", "Человек-неведимка", "Джентельмены", "Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindOverLimit() {
+        MovieManager manager = new MovieManager(15);
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Отель Белград");
+        String[] expected = {"Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLessLimit() {
+        MovieManager manager = new MovieManager(4);
+        manager.addMovie("Бладшот");
+        manager.addMovie("Вперед");
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        String[] expected = {"Джентельмены", "Отель Белград", "Вперед", "Бладшот"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLessLimit2() {
+        MovieManager manager = new MovieManager(2);
+        manager.addMovie("Отель Белград");
+        manager.addMovie("Джентельмены");
+        String[] expected = {"Джентельмены", "Отель Белград"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
